fb86df41b1b8bd29a05014225aac467fd6aa800b
Christoph Fischer <chris@toph.de>
2014-02-19 15:21:09 +0100

    Initial BE module setup

--- ext_tables.php
+++ ext_tables.php
@@ -4,27 +4,43 @@ if (!defined('TYPO3_MODE')) {
 }
 
 if (TYPO3_MODE === 'BE') {
-
-	/**
-	 * Registers a Backend Module
-	 */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'TYPO3.' . $_EXTKEY,
-		'vmfdsmodule',	 // Make module a submodule of 'web'
-		'',	// Submodule key
-		'',						// Position
-		array(
-			
-		),
-		array(
-			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_vmfdsmodule.xlf',
-		)
-	);
-
+     
+    $modulName = 'VolksmissionModule';
+ 
+    //Legt die Position des Moduls fest, hier nach Modul "web"
+    if (!isset($TBE_MODULES[$modulName])) {
+        $temp_TBE_MODULES = array();
+        foreach ($TBE_MODULES as $key => $val) {
+            if ($key == 'web') {
+                $temp_TBE_MODULES[$key] = $val;
+                $temp_TBE_MODULES[$modulName] = '';
+            } else {
+                $temp_TBE_MODULES[$key] = $val;
+            }
+        }
+        $TBE_MODULES = $temp_TBE_MODULES;
+    }
+    // Hauptmodul erstellen
+    t3lib_extMgm::addModule($modulName, '', '', t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/BackendModul/');
+ 
+    /**
+     * Registers a Backend Modul
+     */
+    Tx_Extbase_Utility_Extension::registerModule(
+        $_EXTKEY,
+        'VolksmissionModule',   //Hier muss der Name aus Zeile 3 eingesetzt werden!
+        'kategory', //
+        '',     // Position
+        array(
+            ....
+        ),
+        array(
+            'access' => 'user,group',
+            'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_kategory.xml',
+        )
+    );
 }
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Volksmissionsmodul');
 
 ?>
